e2108f281b4d5f11f4b941c4ed3b5d50a2d9ede1
Lindsey C <lindseyacason@gmail.com>
2020-01-22 12:20:25 -0600

    updating

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,14 +6,16 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        while cur_index < len(arr):
+            #until we get to the end of the arrays indicies
+            #if the left number is smaller, set index to smaller index
+            if arr[cur_index] < arr[smallest_index]:
+                smallest_index = cur_index
+                cur_index = cur_index +1
 
-
-
-        # TO-DO: swap
-
-
-
+        #complete the swap
+        #make these two on the left, now equal to the ones on the right in reverse order
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -21,6 +23,19 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    #set initial swap status
+    swap = True
+    while swap:
+        #toggle swap
+        swap=False
+        for i in range(0,len(arr)-1):
+            left=arr[i]
+            right=arr[i+1]
+        #if left is less, swap 'em
+            if left > right:
+                arr[i] = right
+                arr[i+1]=left
+                swap = True
     return arr
 
 
